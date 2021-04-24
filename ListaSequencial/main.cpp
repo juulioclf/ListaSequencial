63a29b30a7999242aa8e8b15161935709a5f009c
juulioclf <juliolima731@gmail.com>
2021-04-24 16:16:06 -0400

    Update main.cpp
    adicionando um pouco de linhas, que imprime 9 funções que o usuário pode realizar dentro de um looping

--- ListaSequencial/main.cpp
+++ ListaSequencial/main.cpp
@@ -1,9 +1,34 @@
+#include <stdlib.h>
+#include <new>
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void limparTela(){
+    system("CLS");
+}
+
+int main(){
+
+    int funcaoUsuario = 0;
+
+    while(funcaoUsuario != 9 && funcaoUsuario > -1){
+
+
+        cout << "funcao 1: \n";
+        cout << "funcao 2: \n";
+        cout << "funcao 3: \n";
+        cout << "funcao 4: \n";
+        cout << "funcao 5: \n";
+        cout << "funcao 6: \n";
+        cout << "funcao 7: \n";
+        cout << "funcao 8: \n";
+        cout << "funcao 9: \n";
+        cout << "digite a opcao desejada: ";
+        cin >> funcaoUsuario;
+        limparTela();
+
+    }
+
     return 0;
 }
