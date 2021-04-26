0c82a4dc56c311ca362a08cd3b653a66cf130c9a
juulioclf <juliolima731@gmail.com>
2021-04-26 10:03:13 -0400

    update
    adcionando a funcao de inserir um usuario no final da lista

--- ListaSequencial/main.cpp
+++ ListaSequencial/main.cpp
@@ -53,6 +53,22 @@ void addComecoSequencial(pessoa *&ponteiro, int *tamanho, string nome, int rg){
 
 }
 
+void addFinalSequencial(pessoa *&ponteiro, int *tamanho, string nome, int rg){
+
+    pessoa *novoSequencial = new pessoa[*tamanho + 1];
+
+    for(int i = 0; i < *tamanho; i++){
+        novoSequencial[i].nome = ponteiro[i].nome;
+        novoSequencial[i].rg = ponteiro[i].rg;
+    }
+
+    novoSequencial[*tamanho].nome = nome;
+    novoSequencial[*tamanho].rg = rg;
+
+    *tamanho = *tamanho + 1;
+
+    ponteiro = novoSequencial;
+}
 
 int main(){
 
@@ -64,8 +80,9 @@ int main(){
 
     while(funcaoUsuario != 9 && funcaoUsuario > -1){
 
+        imprimeSequencial(ponteiroSequencial, tamanho);
         cout << "funcao 1: insercao de uma pessoa no inicio da lista\n";
-        cout << "funcao 2: \n";
+        cout << "funcao 2: insercao de uma pessoa no final da lista\n";
         cout << "funcao 3: \n";
         cout << "funcao 4: \n";
         cout << "funcao 5: \n";
@@ -93,27 +110,49 @@ int main(){
                 cout << "\nUsuario: " << nome << ", RG: " << rg << " Adicionado ao inicio da lista com sucesso!\n";
 
                 break;
+
             case 2:
-                cout << "Voce selecionou a funcao 2:  \n\n";
+
+                cout << "Voce selecionou a funcao 2: insercao de uma pessoa no final da lista\n\n";
+                cout << "Digite um nome: ";
+                cin >> nome;
+                cout << "Digite um RG: ";
+                cin >> rg;
+
+                if(tamanho == 0){
+                    addComecoSequencial(ponteiroSequencial, &tamanho, nome, rg);
+                }else{
+                    addFinalSequencial(ponteiroSequencial, &tamanho, nome, rg);
+                }
+
+                cout << "\nUsuario: " << nome << ", RG: " << rg << " Adicionado ao final da lista com sucesso!\n";
+
                 break;
+
             case 3:
                 cout << "Voce selecionou a funcao 3:  \n\n";
                 break;
+
             case 4:
                 cout << "Voce selecionou a funcao 4:  \n\n";
                 break;
+
             case 5:
                 cout << "Voce selecionou a funcao 5:  \n\n";
                 break;
+
             case 6:
                 cout << "Voce selecionou a funcao 6:  \n\n";
                 break;
+
             case 7:
                 cout << "Voce selecionou a funcao 7:  \n\n";
                 break;
+
             case 8:
                 cout << "Voce selecionou a funcao 8:  \n\n";
                 break;
+
             case 9:
                 cout << "Voce selecionou a funcao 9:  \n\n";
                 break;
