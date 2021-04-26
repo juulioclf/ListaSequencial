cec5c0abab1275c465a79b2a12dc2349146baf30
juulioclf <juliolima731@gmail.com>
2021-04-26 07:31:35 -0400

    Update
    Adicionando funcao de adicionar um usuario no comeco da lista sequencial

--- ListaSequencial/main.cpp
+++ ListaSequencial/main.cpp
@@ -19,36 +19,52 @@ struct pessoa{
 void imprimeSequencial(pessoa *ponteiro, int tamanho){
 
     for(int i = 0; i < tamanho; i++){
-        cout << "linha: " << i << " nome: " << ponteiro[i].nome <<" rg: " << ponteiro[i].rg << "\n";
+        cout << i << "-nome: " << ponteiro[i].nome <<" rg: " << ponteiro[i].rg << "\n";
     }
+}
+
+void addComecoSequencial(pessoa *&ponteiro, int *tamanho, string nome, int rg){
+
+    if(*tamanho == 0){
+
+        pessoa *novoSequencial = new pessoa[1];
+
+        novoSequencial[0].nome = nome;
+        novoSequencial[0].rg = rg;
+
+        ponteiro = novoSequencial;
+
+    }else{
+        pessoa *novoSequencial = new pessoa[*tamanho + 1];
+
+        novoSequencial[0].nome = nome;
+        novoSequencial[0].rg = rg;
+
+        for(int i = 0; i < *tamanho; i++){
+            novoSequencial[i+1].nome = ponteiro[i].nome;
+            novoSequencial[i+1].rg = ponteiro[i].rg;
+        }
+
+        ponteiro = novoSequencial;
+
+    }
+
+    *tamanho = *tamanho + 1;
+
 }
 
 
 int main(){
 
     int funcaoUsuario = 0;
-    int tamanho;
+    int tamanho = 0;
 
     pessoa *ponteiroSequencial;
 
 
-    //exemplo:
-
-    pessoa *exemploSequencial = new pessoa[2];
-
-    exemploSequencial[0].nome = "julio";
-    exemploSequencial[0].rg = 123;
-    exemploSequencial[1].nome = "gabe";
-    exemploSequencial[1].rg = 321;
-
-    ponteiroSequencial = exemploSequencial;
-
-    imprimeSequencial(ponteiroSequencial, 2);
-
-
     while(funcaoUsuario != 9 && funcaoUsuario > -1){
 
-        cout << "funcao 1: \n";
+        cout << "funcao 1: insercao de uma pessoa no inicio da lista\n";
         cout << "funcao 2: \n";
         cout << "funcao 3: \n";
         cout << "funcao 4: \n";
@@ -57,9 +73,43 @@ int main(){
         cout << "funcao 7: \n";
         cout << "funcao 8: \n";
         cout << "funcao 9: \n";
-        cout << "digite a opcao desejada: ";
+        cout << "\ndigite a opcao desejada e pressione ENTER: ";
         cin >> funcaoUsuario;
         limparTela();
+
+        string nome;
+        int rg;
+
+        switch (funcaoUsuario){
+            case 1:
+                cout << "Voce selecionou a funcao 1:  insercao de uma pessoa no inicio da lista\n\n";\
+                cout << "Digite um nome: ";
+                cin >> nome;
+                cout << "Digite um RG: ";
+                cin >> rg;
+
+                addComecoSequencial(ponteiroSequencial, &tamanho, nome, rg);
+
+                cout << "\nUsuario: " << nome << ",RG: " << rg << " Adicionado ao inicio da lista com sucesso!\n";
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+        }
 
     }
 
